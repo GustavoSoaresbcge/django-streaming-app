f95fb67e47001f9c067c2abbaaf44edaa80fc70b
Gustavo Soares <gustavosoaresbcge@gmail.com>
2025-07-15 20:37:07 -0300

    update csrf

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3--lnnh78j@1-y+%!(9@-t#gt_go^njs!j%a)a_9_vz12wy03_'
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://django-streaming-app-production.up.railway.app']
+else:
+    SECRET_KEY = 'django-insecure-3--lnnh78j@1-y+%!(9@-t#gt_go^njs!j%a)a_9_vz12wy03_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://django-streaming-app-production.up.railway.app/", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -87,7 +94,6 @@ DATABASES = {
 }
 
 import dj_database_url
-import os
 
 DATABASE_URL = 'postgresql://postgres:OByorFbhgGgvQxEfEUQQJMrmZencKWio@postgres.railway.internal:5432/railway'
 if DATABASE_URL:
