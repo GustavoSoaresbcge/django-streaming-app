87b7c584990a5853209d204627c379cbe81441b3
Gustavo Soares <gustavosoaresbcge@gmail.com>
2025-07-15 01:49:31 -0300

    deploy05

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgresql://postgres:OByorFbhgGgvQxEfEUQQJMrmZencKWio@postgres.railway.internal:5432/railway'
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
